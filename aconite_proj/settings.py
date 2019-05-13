b09d48c09cb6adffe98a68dc884097331b892202
sologuboved <sologuboved@gmail.com>
2019-05-13 18:13:19 +0300

    updated settings

--- aconite_proj/settings.py
+++ aconite_proj/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'aconite_app.apps.AconiteAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'aconite_proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': secret_data.DB_NAME,
+        'USER': secret_data.DB_USERNAME,
+        'PASSWORD': secret_data.DB_PASSWORD,
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
