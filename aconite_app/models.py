3d1f5b82b5046e9e25c68d41d6258aba2875d26e
sologuboved <sologuboved@gmail.com>
2019-05-16 00:50:49 +0300

    updated models & migrations

--- aconite_app/models.py
+++ aconite_app/models.py
@@ -2,30 +2,30 @@ from django.db import models
 
 
 class Language(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=10)
 
     def __str__(self):
         return self.name
 
 
 class Location(models.Model):
-    ru_name = models.CharField()
-    en_name = models.CharField()
+    ru_name = models.CharField(max_length=100)
+    en_name = models.CharField(max_length=100)
 
     def __str__(self):
         return "{} / {}".format(self.ru_name, self.en_name)
 
 
 class Person(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=250)
 
     def __str__(self):
         return self.name
 
 
 class Genre(models.Model):
-    ru_name = models.CharField()
-    en_name = models.CharField()
+    ru_name = models.CharField(max_length=100)
+    en_name = models.CharField(max_length=100)
 
     def __str__(self):
         return "{} / {}".format(self.ru_name, self.en_name)
@@ -33,29 +33,27 @@ class Genre(models.Model):
 
 class Category(models.Model):
     is_prose = models.BooleanField()
-    genre = models.ManyToManyField(
-        Genre,
+    genres = models.ManyToManyField(
+        Genre
     )
 
     def __str__(self):
         return "{} {}".format(type(self.is_prose), self.is_prose)
-        # return "Is{}prose: {}".format({True: " ", False: " not "}[self.is_prose], self.genre)
+        # return "{}rosaic: {}".format({True: "P, False: "Non-p"}[self.is_prose], self.genre)
 
 
 class Content(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=3000)
     text = models.TextField()
     num_part = models.IntegerField()
     day = models.IntegerField()
     month = models.IntegerField()
     year = models.IntegerField()
-    location = models.ManyToManyField(
-        Location,
-        null=True
+    locations = models.ManyToManyField(
+        Location
     )
-    language = models.ManyToManyField(
-        Language,
-        null=True
+    languages = models.ManyToManyField(
+        Language
     )
     work = models.ForeignKey(
         'Work',
@@ -67,27 +65,34 @@ class Content(models.Model):
         return "{}:({}) {}".format(self.work, self.num_part, self.title)
 
 
-class Work(models.Model):
-    title = models.CharField()
+class Book(models.Model):
+    title = models.CharField(max_length=3000)
     author = models.ManyToManyField(
-        Person,
+        Person
+    )
+
+    def __str__(self):
+        return "{} - {}".format(self.author, self.title)
+
+
+class Inspiration(Book):
+    # works: reverse relation to Work
+
+    def __str__(self):
+        return "{} - {}".format(self.author, self.title)
+
+
+class Work(Book):
+    is_translation = models.BooleanField()
+    inspirations = models.ManyToManyField(
+        Inspiration
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
         null=True
     )
     # contents: reverse relation to Content
 
     def __str__(self):
         return "{} - {}".format(self.author, self.title)
-
-
-class Inspiration(models.Model):
-    pass
-
-
-
-# category = models.ForeignKey(
-#     Category,
-#     on_delete=models.CASCADE,
-#     null=True
-# )
-
-
