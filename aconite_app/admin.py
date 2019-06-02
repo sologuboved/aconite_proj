3fef8ccb3ca88776218e85af8c49b4da3a203f35
sologuboved <sologuboved@gmail.com>
2019-06-02 22:08:15 +0300

    updated admin

--- aconite_app/admin.py
+++ aconite_app/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from aconite_app.models import *
 
-# Register your models here.
+
+class LanguageInline(admin.TabularInline):
+    model = Work.languages.through
+
+
+class LocationInline(admin.TabularInline):
+    model = Work.locations.through
+
+
+class InspirationInline(admin.TabularInline):
+    model = Work.inspirations.through
+
+
+class WorkAdmin(admin.ModelAdmin):
+    inlines = [LanguageInline, LocationInline, InspirationInline]
+    fields = ['title', 'authors', 'original_title', 'dedication', 'year_demo', 'year', 'month', 'day', 'is_translation',
+              'genre']
+
+
+class ContentAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Work, WorkAdmin)
+admin.site.register(Content, ContentAdmin)
+
